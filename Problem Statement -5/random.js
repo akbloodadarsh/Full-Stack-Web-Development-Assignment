08414e37773074bdf5620b342f463d67c2916982
Adarsh Kunwar <adarsh.infinite@gmail.com>
2020-03-16 22:46:16 +0530

    completed the problem statement 5

--- Problem Statement -5/random.js
+++ Problem Statement -5/random.js
@@ -1,6 +1,28 @@
 var rand =  Math.floor(Math.random()*101);
 a = [];
 
+function new_element(){
+	num = document.getElementById('array').value;
+	n = a.push(num);
+	if(n == 10 || num == rand)
+	{
+		if(n == 10)
+			{
+				next_time();
+			}
+		if(num == rand)
+			{
+				win();
+			}
+			exit();
+	}
+	document.getElementById('show').innerHTML = "";
+	for(x in a)
+	{
+		document.getElementById('show').innerHTML += a[x];
+		document.getElementById('show').innerHTML += " ";	
+	}
+}
 function next_time()
 {
 	alert("Maybe next time universe is gonna be with you");
@@ -14,20 +36,4 @@ function win()
 function exit()
 {
 	location.reload();
-}
-
-function new_element(){
-	num = document.getElementById('array');
-	n = a.push(num);
-	if(n == 10 || num == rand)
-	{
-		if(n == 10)next_time();
-		else win();
-		exit();
-	}
-	for(x in a)
-	{
-		console.log(document.getElementById('array'));
-		// document.getElementById('show') + x + ' ';	
-	}
 }
